69229136ac4d46b8e2e8ef7c29fff1255a77faa6
Markus Lindgren <markus.lindgren.8800@student.uu.se>
2023-12-18 15:11:06 -0700

    gpt prompting

---
+++ subtaskA/models/gpt.py
@@ -0,0 +1,31 @@
+import os
+from openai import OpenAI
+import pandas as pd
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+client = OpenAI()
+
+OpenAI.api_key = os.getenv('OPENAI_API_KEY')
+
+test_df = pd.read_json('subtaskA/data/subtaskA_dev_monolingual.jsonl', lines=True)    
+
+output_file = 'subtaskA/predictions/gpt.json'  
+with open(output_file, 'w', encoding='utf-8') as f:
+    for idx, ex in enumerate(test_df['text'][:10]):
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-16k",
+            messages=[
+                {"role": "system", "content": "You are a language model designed to determine if the following text is written by a human or a machine."},
+                {"role": "user", "content": f"Determine if this text is written by a human or a machine. Answer with a single word, 'human' or 'machine'\n\n{ex}"},
+            ]
+        )
+
+        model_output = response.choices[0].message.content
+        print(model_output)
+        prediction = 1 if "machine" in model_output.lower() else 0
+
+        output_line = f'{{"id": {int(test_df.loc[idx, "id"])}, "label": {prediction}}}\n'
+        f.write(output_line)
+
+print(f'Predictions saved to {output_file}')
